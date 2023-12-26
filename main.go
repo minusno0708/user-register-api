d7f4c4e843b5f3c6399d02bb98a764a5c4fd2bed
minusno0708 <naoyao1014@gmail.com>
2023-12-26 21:05:40 +0900

    main.goをmasterブランチの内容に修正

--- main.go
+++ main.go
@@ -5,12 +5,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type User struct {
-	UserID   string `json:"user_id"`
-	Username string `json:"username"`
-	Password string `json:"password"`
-}
-
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
@@ -18,28 +12,5 @@ func main() {
 			"message": "Connection Successful",
 		})
 	})
-	r.POST("/signin", func(c *gin.Context) {
-		var user User
-		if err := c.ShouldBindJSON(&user); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"message": "Bad Request",
-			})
-			return
-		}
-		if user.UserID == "" || user.Password == "" {
-			c.JSON(http.StatusUnauthorized, gin.H{
-				"message": "Unauthorized",
-			})
-			return
-		}
-		if user.Username == "" {
-			user.Username = user.UserID
-		}
-		c.JSON(http.StatusOK, gin.H{
-			"message": "User created successfully",
-			"user": user,
-		})
-	})
-
 	r.Run(":8080")
 }
