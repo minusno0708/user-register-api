3c19cd5794ebb4f57a4233f30d2e778d15fb30b7
minusno0708 <naoyao1014@gmail.com>
2024-01-01 02:48:39 +0900

    test:body無しでのsigninのテストを実装

--- main_test.go
+++ main_test.go
@@ -7,8 +7,16 @@ import (
 	"testing"
 )
 
+const endpoint = "http://localhost:8080"
+
+var response struct {
+	Message string `json:"message"`
+}
+
 func TestConnectionApi(t *testing.T) {
-	resp, err := http.Get("http://localhost:8080/")
+	expectedMessage := "Connection Successful"
+
+	resp, err := http.Get(endpoint)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -17,17 +25,42 @@ func TestConnectionApi(t *testing.T) {
 	}
 	responseData, _ := ioutil.ReadAll(resp.Body)
 
-	var data struct {
-		Message string `json:"message"`
-	}
-
-	err = json.Unmarshal(responseData, &data)
+	err = json.Unmarshal(responseData, &response)
 	if err != nil {
 		t.Fatal(err)
 	}
-	expectedMessage := "Connection Successful"
 
-  	if data.Message != expectedMessage {
-		t.Fatalf("Expected message %v, got %v", expectedMessage, data.Message)
+  	if response.Message != expectedMessage {
+		t.Fatalf("Expected message %v, got %v", expectedMessage, response.Message)
 	}
+}
+
+func TestSigninBodyNotExist(t *testing.T) {
+	expectedMessage := "Body is not valid"
+
+	req, err := http.NewRequest("POST", endpoint+"/signin", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode != http.StatusBadRequest {
+		t.Fatalf("Expected status code 400, got %v", resp.StatusCode)
+	}
+
+	responseData, _ := ioutil.ReadAll(resp.Body)
+
+	err = json.Unmarshal(responseData, &response)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if response.Message != expectedMessage {
+		t.Fatalf("Expected message %v, got %v", expectedMessage, response.Message)
+	}
+	
 }
