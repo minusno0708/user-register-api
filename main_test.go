8df2d912a77277547e3c0ee8d5c1d2371315ce20
minusno0708 <naoyao1014@gmail.com>
2024-01-01 17:14:11 +0900

    test:signinのpassなしテストを実装

--- main_test.go
+++ main_test.go
@@ -106,3 +106,45 @@ func TestSigninUserIDNotExist(t *testing.T) {
 		t.Fatalf("Expected message %v, got %v", expectedMessage, response.Message)
 	}
 }
+
+func TestSigninPasswordNotExist(t *testing.T) {
+	expectedMessage := "Body is not valid"
+
+	requestBody := &User{
+		UserID: "testuser",
+		Username: "testuser",
+	}
+
+	jsonString, err := json.Marshal(requestBody)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req, err := http.NewRequest("POST", endpoint+"/signin", bytes.NewBuffer(jsonString))
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode != http.StatusUnauthorized {
+		t.Fatalf("Expected status code 401, got %v", resp.StatusCode)
+	}
+
+	responseData, _ := ioutil.ReadAll(resp.Body)
+
+	err = json.Unmarshal(responseData, &response)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if response.Message != expectedMessage {
+		t.Fatalf("Expected message %v, got %v", expectedMessage, response.Message)
+	}
+}
