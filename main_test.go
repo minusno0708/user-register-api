c66539c5b452f058959fe8448bbd57e42d817b0d
minusno0708 <naoyao1014@gmail.com>
2024-01-01 20:05:56 +0900

    test:signinの成功テストを実装

--- main_test.go
+++ main_test.go
@@ -12,6 +12,7 @@ const endpoint = "http://localhost:8080"
 
 var response struct {
 	Message string `json:"message"`
+	User User `json:"user"`
 }
 
 func TestConnectionApi(t *testing.T) {
@@ -146,5 +147,108 @@ func TestSigninPasswordNotExist(t *testing.T) {
 
 	if response.Message != expectedMessage {
 		t.Fatalf("Expected message %v, got %v", expectedMessage, response.Message)
+	}
+}
+
+func TestSigninSuccessUsernameExist(t *testing.T) {
+	expectedMessage := "User created successfully"
+
+	requestBody := &User{
+		UserID: "testuser",
+		Username: "testuser",
+		Password: "testpass",
+	}
+
+	jsonString, err := json.Marshal(requestBody)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req, err := http.NewRequest("POST", endpoint+"/signin", bytes.NewBuffer(jsonString))
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("Expected status code 200, got %v", resp.StatusCode)
+	}
+
+	responseData, _ := ioutil.ReadAll(resp.Body)
+	err = json.Unmarshal(responseData, &response)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if response.Message != expectedMessage {
+		t.Fatalf("Expected message %v, got %v", expectedMessage, response.Message)
+	}
+
+	if response.User.UserID != requestBody.UserID {
+		t.Fatalf("Expected user_id %v, got %v", requestBody.UserID, response.User.UserID)
+	}
+	if response.User.Username != requestBody.Username {
+		t.Fatalf("Expected username %v, got %v", requestBody.Username, response.User.Username)
+	}
+	if response.User.Password != requestBody.Password {
+		t.Fatalf("Expected password %v, got %v", requestBody.Password, response.User.Password)
+	}
+}
+
+func TestSigninSuccessUsernameNotExist(t *testing.T) {
+	expectedMessage := "User created successfully"
+
+	requestBody := &User{
+		UserID: "testuser",
+		Password: "testpass",
+	}
+
+	jsonString, err := json.Marshal(requestBody)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req, err := http.NewRequest("POST", endpoint+"/signin", bytes.NewBuffer(jsonString))
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("Expected status code 200, got %v", resp.StatusCode)
+	}
+
+	responseData, _ := ioutil.ReadAll(resp.Body)
+	err = json.Unmarshal(responseData, &response)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if response.Message != expectedMessage {
+		t.Fatalf("Expected message %v, got %v", expectedMessage, response.Message)
+	}
+
+	if response.User.UserID != requestBody.UserID {
+		t.Fatalf("Expected user_id %v, got %v", requestBody.UserID, response.User.UserID)
+	}
+	if response.User.Username != requestBody.UserID {
+		t.Fatalf("Expected username %v, got %v", requestBody.UserID, response.User.Username)
+	}
+	if response.User.Password != requestBody.Password {
+		t.Fatalf("Expected password %v, got %v", requestBody.Password, response.User.Password)
 	}
 }
