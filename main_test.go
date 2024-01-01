f75a89b086cb2fbb529c9fee9ed0639e2e7789dd
minusno0708 <naoyao1014@gmail.com>
2024-01-01 17:13:05 +0900

    test:signinのidなしテストを実装

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"net/http"
 	"io/ioutil"
 	"encoding/json"
@@ -61,6 +62,47 @@ func TestSigninBodyNotExist(t *testing.T) {
 
 	if response.Message != expectedMessage {
 		t.Fatalf("Expected message %v, got %v", expectedMessage, response.Message)
+	}	
+}
+
+func TestSigninUserIDNotExist(t *testing.T) {
+	expectedMessage := "Body is not valid"
+
+	requestBody := &User{
+		Username: "testuser",
+		Password: "testpass",
 	}
-	
+
+	jsonString, err := json.Marshal(requestBody)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req, err := http.NewRequest("POST", endpoint+"/signin", bytes.NewBuffer(jsonString))
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode != http.StatusUnauthorized {
+		t.Fatalf("Expected status code 401, got %v", resp.StatusCode)
+	}
+
+	responseData, _ := ioutil.ReadAll(resp.Body)
+
+	err = json.Unmarshal(responseData, &response)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if response.Message != expectedMessage {
+		t.Fatalf("Expected message %v, got %v", expectedMessage, response.Message)
+	}
 }
