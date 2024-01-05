421bc005b53d260fafa632a300fc0105b418394a
minusno0708 <naoyao1014@gmail.com>
2024-01-06 02:11:49 +0900

    refactor:interface層を作成

---
+++ interfaces/handler/user.go
@@ -0,0 +1,52 @@
+package interfaces
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+
+	"user-register-api/config"
+	"user-register-api/domain"
+	"user-register-api/infrastructure"
+)
+
+func HandleUserSignin(c *gin.Context) {
+	var user domain.User
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "Body does not exist",
+		})
+		return
+	}
+	if user.UserID == "" || user.Password == "" {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"message": "Body is not valid",
+		})
+		return
+	}
+	if user.Username == "" {
+		user.Username = user.UserID
+	}
+
+	db, err := config.ConnectDB()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Database connection error",
+		})
+		return
+	}
+	defer db.Close()
+
+	err = infrastructure.InsertUser(db, user)
+	if err != nil {
+		c.JSON(http.StatusConflict, gin.H{
+			"message": "User already exists",
+		})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"message": "User created successfully",
+		"user": user,
+	})
+}
